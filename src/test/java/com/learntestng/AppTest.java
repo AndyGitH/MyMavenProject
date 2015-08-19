7b54e43aa1bff794e8755812470b53d5b3e61488
Andrew <andrew.sagoe@gmailcom>
2015-08-19 18:20:23 +0100

    mavenproject

---
+++ src/test/java/com/learntestng/AppTest.java
@@ -0,0 +1,22 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+
+//run java File from command prompt - Maven
+//run testNG from Maven cmd line
+//surefire reports with Maven
+//xslt reports with Maven
+
+public class AppTest {
+	
+	@Test
+	public void sendMail(){
+		System.out.println("******************App Test******************");
+		System.out.println("******************App Test******************");
+		System.out.println("******************App Test******************");
+		System.out.println("******************App Test******************");
+		System.out.println("******************App Test******************");
+	}
+   
+}
---
+++ src/test/java/com/learntestng/SampleTest.java
@@ -0,0 +1,18 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleTest(){
+		System.out.println("******************Sample Test******************");
+		System.out.println("******************Sample Test******************");
+		System.out.println("******************Sample Test******************");
+		System.out.println("******************Sample Test******************");
+		System.out.println("******************Sample Test******************");
+		System.out.println("******************Sample Test******************");
+	}
+	
+	//mvn test site
+}
